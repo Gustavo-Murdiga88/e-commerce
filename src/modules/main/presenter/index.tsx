88e7b5ebda2afe97af771ac3abafae1c637dd4be
Gustavo <gumurdiga@gmail.com>
2023-09-05 21:39:21 -0300

    feat: add first commits

---
+++ src/modules/main/presenter/index.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+	return (
+		<div className="max-w-page transition-colors pb-20 bg-white hover:bg-slate-800 rounded-md mx-auto flex items-center justify-center gap-10 flex-col pt-10">
+			<h1 className="">Oi Vini</h1>
+			<div className="text-black text-lg flex gap-6">
+				<p>oi Gu</p>
+				<p>oi Lu</p>
+				<p>oi Isa</p>
+				<p>oi Gui</p>
+			</div>
+		</div>
+	);
+}
